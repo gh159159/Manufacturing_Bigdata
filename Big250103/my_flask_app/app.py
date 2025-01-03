f184eb11d657010ceeff92ce5179dac79134d5d2
gh159159 <144233028+gh159159@users.noreply.github.com>
2025-01-03 23:00:22 +0900

    init

--- Big250103/my_flask_app/app.py
+++ Big250103/my_flask_app/app.py
@@ -4,7 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("mainpage.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/id-class")
+def idclass():
+    return render_template("id-class.html")
+
+@app.route("/layout")
+def layout():
+    return render_template("layout.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
